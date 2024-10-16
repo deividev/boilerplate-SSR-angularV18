160af0804d6d278a838d06830fd26c302328fa1c
davids <deivid.develop@gmail.com>
2024-10-16 12:31:26 +0200

    chore: remove code commento to test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,11 +24,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Angular18-boilerplate');
   });
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular18');
-  // });
 });
